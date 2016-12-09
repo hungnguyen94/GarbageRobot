73d2527dfbc3c183b88709de713fb89ac2bbdce0
Hung Nguyen <H.Nguyen-3@student.tudelft.nl>
2016-12-09 15:20:56 +0100

    Fix rounding

--- oscar_garbage_classifier/src/train_squeezenet.py
+++ oscar_garbage_classifier/src/train_squeezenet.py
@@ -16,23 +16,25 @@ def load_image(img_path):
 
     # Crop image to a square
     height, width, depth = img.shape
-    offset = min(height, width) / 2.0
-    center_x, center_y = (width / 2.0, height / 2.0)
+    offset = int(round(min(height, width) / 2.0))
+    center_x = int(round(width / 2.0))
+    center_y = int(round(height / 2.0))
     cropped_img = img[center_y - offset: center_y + offset, center_x - offset: center_x + offset]
 
     # Resize image to 227, 227 as Squeezenet only accepts this format.
-    resized_image = cv2.resize(cropped_img, (227, 227)).astype(np.float64)
+    resized_image = cv2.resize(cropped_img, (227, 227)).astype(np.float32)
     return resized_image
 
 # List comprehensions. Create a list of two tuples with (images, class).
+print('Loading images..')
 images_classes = [(load_image(os.path.join(subdir, f)), os.path.basename(subdir))
                   for subdir, dirs, files in os.walk(images_dir)
                   for f in files]
 # Unzip to two lists.
 images, classes = zip(*images_classes)
 nr_classes = len(set(classes))
-print('Loaded images..')
 
+print('Loading model..')
 model = get_squeezenet(nr_classes, dim_ordering='tf')
 model.compile(loss="categorical_crossentropy", optimizer="adam")
 print("Fitting model")
