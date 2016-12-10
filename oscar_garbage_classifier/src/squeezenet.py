7bccc30fa686a26e78fbbebf3ecedc254a938497
Hung Nguyen <H.Nguyen-3@student.tudelft.nl>
2016-12-10 14:01:47 +0100

    Add own squeezenet implementation

--- oscar_garbage_classifier/src/squeezenet.py
+++ oscar_garbage_classifier/src/squeezenet.py
@@ -1,99 +1,67 @@
-from keras.layers import Input, merge
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Dropout, Activation
-from keras.layers.pooling import GlobalAveragePooling2D
+import h5py
 from keras.models import Model
+from keras.layers import Input, Convolution2D, MaxPooling2D, Dropout, GlobalAveragePooling2D, merge, Activation
 
-sq1x1 = "squeeze1x1"
-exp1x1 = "expand1x1"
-exp3x3 = "expand3x3"
-relu = "relu_"
+def FireModule(s_1x1, e_1x1, e_3x3, name):
+    """
+        Fire module for the SqueezeNet model. 
+        Implements the expand layer, which has a mix of 1x1 and 3x3 filters, 
+        by using two conv layers concatenated in the channel dimension. 
 
-# Modular function for Fire Node
-
-def fire_module(x, fire_id, squeeze=16, expand=64, dim_ordering='tf'):
-    s_id = 'fire' + str(fire_id) + '/'
-    if dim_ordering is 'tf':
-        c_axis = 3
-    else:
-        c_axis = 1
-
-    x = Convolution2D(squeeze, 1, 1, border_mode='valid', name=s_id + sq1x1)(x)
-    x = Activation('relu', name=s_id + relu + sq1x1)(x)
-
-    left = Convolution2D(expand, 1, 1, border_mode='valid', name=s_id + exp1x1)(x)
-    left = Activation('relu', name=s_id + relu + exp1x1)(left)
-
-    right = Convolution2D(expand, 3, 3, border_mode='same', name=s_id + exp3x3)(x)
-    right = Activation('relu', name=s_id + relu + exp3x3)(right)
-
-    x = merge([left, right], mode='concat', concat_axis=c_axis, name=s_id + 'concat')
-    return x
+        Returns a callable function
+    """
+    def layer(x):
+        squeeze = Convolution2D(s_1x1, 1, 1, activation='relu', name=name+'_squeeze')(x)
+        # Set border_mode to same to pad output of expand_3x3 with zeros.
+        # Needed to merge layers expand_1x1 and expand_3x3.
+        expand_1x1 = Convolution2D(e_1x1, 1, 1, border_mode='same', activation='relu', name=name+'_expand_1x1')(squeeze)
+        expand_3x3 = Convolution2D(e_3x3, 3, 3, border_mode='same', activation='relu', name=name+'_expand_3x3')(squeeze)
+        expand_merge = merge([expand_1x1, expand_3x3], mode='concat', concat_axis=3, name=name+'_expand_merge')
+        return expand_merge
+    return layer
+    
 
 
-# Original SqueezeNet from paper.
+def SqueezeNet(nb_classes): 
+    # Use input shape (227, 227, 3) instead of the (224, 224, 3) shape cited in the paper. 
+    # This results in conv1 output shape = (None, 111, 111, 96), same as in the paper. 
+    input_image = Input(shape=(227, 227, 3))
+    conv1 = Convolution2D(96, 7, 7, activation='relu', subsample=(2, 2), name='conv1')(input_image)
+    # maxpool1 output shape = (?, 55, 55, 96)
+    maxpool1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='maxpool1')(conv1)
 
-def get_squeezenet(nb_classes, dim_ordering='tf'):
-    if dim_ordering is 'th':
-        input_img = Input(shape=(3, 227, 227))
-    elif dim_ordering is 'tf':
-        input_img = Input(shape=(227, 227, 3))
-    else:
-        raise NotImplementedError("Theano and Tensorflow are only available")
-    x = Convolution2D(64, 3, 3, subsample=(2, 2), border_mode='valid', name='conv1')(input_img)
-    x = Activation('relu', name='relu_conv1')(x)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
+    # fire2 output shape = (?, 55, 55, 128)
+    fire2 = FireModule(s_1x1=16, e_1x1=64, e_3x3=64, name='fire2')(maxpool1)
+    # fire3 output shape = (?, 55, 55, 128)
+    fire3 = FireModule(s_1x1=16, e_1x1=64, e_3x3=64, name='fire3')(fire2)
+    # fire4 output shape = (?, 55, 55, 256)
+    fire4 = FireModule(s_1x1=32, e_1x1=128, e_3x3=128, name='fire4')(fire3)
+    # maxpool4 output shape = (?, 27, 27, 256)
+    maxpool4 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='maxpool4')(fire4)
+    # fire5 output shape = (?, 27, 27, 384)
+    fire5 = FireModule(s_1x1=32, e_1x1=128, e_3x3=128, name='fire5')(maxpool4)
+    # fire6 output shape = (?, 27, 27, 384)
+    fire6 = FireModule(s_1x1=48, e_1x1=192, e_3x3=192, name='fire6')(fire5)
+    # fire7 output shape = (?, 27, 27, 384)
+    fire7 = FireModule(s_1x1=48, e_1x1=192, e_3x3=192, name='fire7')(fire6)
+    # fire8 output shape = (?, 27, 27, 512)
+    fire8 = FireModule(s_1x1=64, e_1x1=256, e_3x3=256, name='fire8')(fire7)
+    # maxpool8 output shape = (?, 13, 13, 384)
+    maxpool8 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='maxpool8')(fire7)
+    # fire9 output shape = (?, 13, 13, 512)
+    fire9 = FireModule(s_1x1=64, e_1x1=256, e_3x3=256, name='fire9')(maxpool8)
+    # Dropout after fire9 module.
+    fire9_dropout = Dropout(p=0.5, name='fire9_dropout')(fire9)
 
-    x = fire_module(x, fire_id=2, squeeze=16, expand=64, dim_ordering=dim_ordering)
-    x = fire_module(x, fire_id=3, squeeze=16, expand=64, dim_ordering=dim_ordering)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
+    # conv10 output shape = (?, 13, 13, 6)
+    conv10 = Convolution2D(nb_classes, 1, 1, activation='relu')(fire9_dropout)
+    # avgpool10, softmax output shape = (?, nb_classes)
+    avgpool10 = GlobalAveragePooling2D(name='avgpool10')(conv10)
+    # avgpool10 = AveragePooling2D(pool_size=(13, 13), strides=(1, 1), name='avgpool10')(conv10)
+    softmax = Activation('softmax', name='softmax')(avgpool10)
 
-    x = fire_module(x, fire_id=4, squeeze=32, expand=128, dim_ordering=dim_ordering)
-    x = fire_module(x, fire_id=5, squeeze=32, expand=128, dim_ordering=dim_ordering)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
-
-    x = fire_module(x, fire_id=6, squeeze=48, expand=192, dim_ordering=dim_ordering)
-    x = fire_module(x, fire_id=7, squeeze=48, expand=192, dim_ordering=dim_ordering)
-    x = fire_module(x, fire_id=8, squeeze=64, expand=256, dim_ordering=dim_ordering)
-    x = fire_module(x, fire_id=9, squeeze=64, expand=256, dim_ordering=dim_ordering)
-    x = Dropout(0.5, name='drop9')(x)
-
-    x = Convolution2D(nb_classes, 1, 1, border_mode='valid', name='conv10')(x)
-    x = Activation('relu', name='relu_conv10')(x)
-    x = GlobalAveragePooling2D()(x)
-    out = Activation('softmax', name='loss')(x)
-    model = Model(input=input_img, output=[out])
+    model = Model(input=input_image, output=[softmax])
     return model
 
 
 
-
-if __name__ == '__main__':
-    import time
-    from keras.utils.visualize_util import plot
-    from quiver_engine import server
-
-    start = time.time()
-    model = get_squeezenet(1000)
-
-    duration = time.time() - start
-    print "{} s to make model".format(duration)
-
-    start = time.time()
-    model.output
-    duration = time.time() - start
-    print "{} s to get output".format(duration)
-
-    start = time.time()
-    model.compile(loss="categorical_crossentropy", optimizer="adam")
-    duration = time.time() - start
-    print "{} s to get compile".format(duration)
-
-    model.load_weights('/mnt/data/Development/ros/catkin_ws/src/oscar_garbage_classifier/models/squeezenet_weights_tf_dim_ordering_tf_kernels.h5', by_name=True)
-    classes = []
-    with open('/mnt/data/Development/ros/catkin_ws/src/oscar_garbage_classifier/src/classes.txt', 'r') as list_:
-        for line in list_:
-            classes.append(line.rstrip('\n'))
-    server.launch(model, temp_folder='/tmp/quiver', input_folder='/mnt/data/Development/ros/catkin_ws/images')
-
-    # plot(model, to_file='images/SqueezeNet.png', show_shapes=True)
