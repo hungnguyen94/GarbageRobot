2fb584e8cb1e9f0f3c0c1a27cf618b31a9843d2f
Yoeri Appel <yoeri@ofira.nl>
2016-12-17 11:18:08 +0100

    Improved log messages of set_mode service

--- oscar/src/py/set_gpio_mode.py
+++ oscar/src/py/set_gpio_mode.py
@@ -2,16 +2,16 @@
 from oscar.srv import *
 import rospy
 
-def set_gpio_mode(req):
-    rospy.loginfo("Requested received to set ping %s to %s", req.pinNumber, req.mode)
-    return SetGpioModeResponse("Request handled")
+def handle_set_gpio_mode(req):
+    rospy.loginfo("Requested received to set pin %s to %s", req.pinNumber, req.mode)
+    # TODO actually set the gpio pin to desired output
+    response = "Pin %s set to %s"%(req.pinNumber, req.mode)
+    return SetGpioModeResponse(response)
 
 def set_gpio_mode_server():
     rospy.init_node('set_gpio_mode_node')
-    s = rospy.Service('set_gpio_node', SetGpioMode, set_gpio_mode)
+    s = rospy.Service('set_gpio_node', SetGpioMode, handle_set_gpio_mode)
     rospy.spin()
 
 if __name__ == "__main__":
-    rospy.logerr("I will publish to the topic")
-    rospy.loginfo("Starting node set_gpio_mode_node")
     set_gpio_mode_server()
