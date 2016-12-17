9a4445bca3d610605193cc8c4d4a5c143d9dfbfd
Yoeri Appel <yoeri@ofira.nl>
2016-12-17 14:56:35 +0100

    Added a service to set the mode (INPUT/OUTPUT) of a gpio pin

--- oscar/src/py/set_gpio_mode.py
+++ oscar/src/py/set_gpio_mode.py
@@ -3,14 +3,22 @@ from oscar.srv import *
 import rospy
 
 def handle_set_gpio_mode(req):
-    rospy.loginfo("Requested received to set pin %s to %s", req.pinNumber, req.mode)
-    # TODO actually set the gpio pin to desired output
-    response = "Pin %s set to %s"%(req.pinNumber, req.mode)
+    mode = "INPUT" if req.mode==0  else "OUTPUT"
+    rospy.loginfo("Requested received to set pin %s to mode %s", req.pinNumber, mode)
+    # TODO actually set the gpio pin to desired mode
+    response = "Pin %s set to mode %s"%(req.pinNumber, mode)
+    return SetGpioModeResponse(response)
+
+def handle_set_gpio_value(req):
+    rospy.loginfo("Requested received to set pin %s to %s", req.pinNumber, req.value)
+    # TODO actually set the gpio pin to desired value
+    response ="Pin %s set to %s" %(str(req.pinNumber), str(req.value))
     return SetGpioModeResponse(response)
 
 def set_gpio_mode_server():
     rospy.init_node('set_gpio_mode_node')
-    s = rospy.Service('set_gpio_node', SetGpioMode, handle_set_gpio_mode)
+    rospy.Service('set_gpio_mode', SetGpioMode, handle_set_gpio_mode)
+    rospy.Service('set_gpio_value', SetGpioValue, handle_set_gpio_value)
     rospy.spin()
 
 if __name__ == "__main__":
