43c6a729bf45ba6ff2f412ad5784aa70f46a2aba
Hung Nguyen <hungnguyen94@gmail.com>
2016-12-19 13:14:31 +0000

    Added the actual setting of the gpio pins

--- oscar/src/py/gpio_services.py
+++ oscar/src/py/gpio_services.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 from oscar.srv import *
 import rospy
+import RPi.GPIO as GPIO
 
 def handle_set_gpio_mode(req):
-    mode = "INPUT" if req.mode==0  else "OUTPUT"
+    mode = GPIO.IN if req.mode==1  else GPIO.OUT
     rospy.loginfo("Requested received to set pin %s to mode %s", req.pinNumber, mode)
-    # TODO actually set the gpio pin to desired mode
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(req.pinNumber,mode)
     response = "Pin %s set to mode %s"%(req.pinNumber, mode)
     return SetGpioModeResponse(response)
 
 def handle_set_gpio_value(req):
     rospy.loginfo("Requested received to set pin %s to %s", req.pinNumber, req.value)
-    # TODO actually set the gpio pin to desired value
-    response ="Pin %s set to %s" %(str(req.pinNumber), str(req.value))
+    output = GPIO.HIGH if (req.value) else GPIO.LOW
+    GPIO.output(req.pinNumber, output)
+    response ="Pin %s set to %s" %(str(req.pinNumber), str(output))
     return SetGpioModeResponse(response)
 
 def set_gpio_mode_server():
--- oscar_garbage_classifier/src/classify_client.py
+++ oscar_garbage_classifier/src/classify_client.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import rospy
 from oscar_garbage_classifier.srv import ClassifyImage
+import RPi.GPIO as GPIO
 
 def classify_image_client(image):
     rospy.wait_for_service('image_classify')
@@ -19,6 +20,7 @@ if __name__ == '__main__':
     from cv_bridge import CvBridge
     img = cv2.imread('/mnt/data/Development/ros/catkin_ws/images/coffee_cup_7.jpg')
     imgmsg = CvBridge().cv2_to_imgmsg(img)
-    classify_image_client(imgmsg)
+    res = classify_image_client(imgmsg)
+
 
 
