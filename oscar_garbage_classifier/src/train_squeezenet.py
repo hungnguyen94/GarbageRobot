33b1624118cb33f489164889b7a08d40612ce2ba
Hung Nguyen <hungnguyen94@gmail.com>
2017-01-23 12:08:55 +0100

    Change train script

--- oscar_garbage_classifier/src/train_squeezenet.py
+++ oscar_garbage_classifier/src/train_squeezenet.py
@@ -14,22 +14,20 @@ import os
 
 initial_epoch = 0
 nb_epoch = 120
-batch_size = 15
-samples_per_epoch = 700
-nb_val_samples = 100
+batch_size = 16
+samples_per_epoch = 1000
+nb_val_samples = 150
 
-input_shape = (300, 300, 3)
+input_shape = (224, 224, 3)
 width = input_shape[0]
 height = input_shape[1]
 channels = input_shape[2]
 
-training_dir = '../images/training-21_jan'
-training_dir2 = '../images/training_images-processed'
-val_dir = '../images/validation-21_jan'
-val_dir2 = '../images/validation_images-processed'
-save_weights_file = '../models/squeezenet_v1.1_weights_%s_epochs_%sx%s.h5' % (nb_epoch, width, height)
+training_dir = '../images/training-webcam_and_web'
+val_dir = '../images/validation-webcam_and_web'
+save_weights_file = '../models/squeezenet_v1.1_weights_webcam_web_softmax%s_epochs_%sx%s.h5' % (nb_epoch, width, height)
 start_weights_file = '' #save_weights_file
-weights_target = "../models/squeezenet_webcam_weights_%sx%s.{epoch:03d}-loss_{val_loss:.5f}-acc_{val_acc:.5f}.h5" % (width, height)
+weights_target = "../models/squeezenet_webcam_web_weights_%sx%s.{epoch:03d}-loss_{val_loss:.5f}-acc_{val_acc:.5f}.h5" % (width, height)
 
 classes = ['bottles', 'cans', 'cups', 'cups_wrong', 'other']
 nb_classes = len(classes)
@@ -40,12 +38,16 @@ train_datagen = ImageDataGenerator(
         width_shift_range=0.05,
         rotation_range=1.,
         height_shift_range=0.05,
+        samplewise_std_normalization=True,
+        #zca_whitening=True,
         horizontal_flip=False,
         vertical_flip=True,
         fill_mode='constant')
 
 test_datagen = ImageDataGenerator(
         rescale=1./255,
+        samplewise_std_normalization=True,
+        #zca_whitening=True,
         fill_mode='constant')
         
 def set_correct_classes(arr):
@@ -58,18 +60,11 @@ train_generator = train_datagen.flow_from_directory(training_dir,
                                                     batch_size=batch_size,
                                                     class_mode='categorical',
                                                     classes=classes)
-train_generator2 = train_datagen.flow_from_directory(training_dir2,
-                                                     target_size=(width, height),
-                                                     batch_size=batch_size,
-                                                     class_mode='categorical',
-                                                     classes=classes)
 def train_generator3():
     while True:
         x_train, y_train = train_generator.next()
         y_train = np.asarray([set_correct_classes(el) for el in y_train])
         yield (x_train, y_train)
-        #yield train_generator.next()
-        #yield train_generator2.next()
 
 
 print('train datagen class indices: \n%s' % train_generator.class_indices)
@@ -80,11 +75,6 @@ val_generator = test_datagen.flow_from_directory(val_dir,
                                                  class_mode='categorical',
                                                  classes=classes)
 
-val_generator2 = test_datagen.flow_from_directory(val_dir2,
-                                                  target_size=(width, height),
-                                                  batch_size=batch_size,
-                                                  class_mode='categorical',
-                                                  classes=classes)
 
 def val_generator3():
     while True:
@@ -98,7 +88,7 @@ def top_1_categorical_accuracy(y_true, y_pred):
     k = 1
     return top_k_categorical_accuracy(y_true, y_pred, k=k)
 
-checkpoint = ModelCheckpoint(weights_target, monitor='val_loss',
+checkpoint = ModelCheckpoint(weights_target, monitor='val_acc',
                              verbose=1, save_best_only=True,
                              save_weights_only=True, mode='auto')
 
@@ -106,18 +96,18 @@ tensorboard = TensorBoard(log_dir='../logs', histogram_freq=5, write_graph=True,
 
 print('Loading model..')
 model = SqueezeNet(nb_classes, width, height, channels)
-adam = Adam(lr=0.005)
+adam = Adam(lr=0.004)
 rmsprop = RMSprop(lr=0.005)
-model.compile(loss='binary_crossentropy', optimizer=adam, metrics=[top_1_categorical_accuracy, 'accuracy', 'precision', 'recall', 'categorical_crossentropy', 'binary_crossentropy'])
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=[top_1_categorical_accuracy, 'accuracy', 'precision', 'recall', 'categorical_crossentropy', 'binary_crossentropy'])
 if os.path.isfile(start_weights_file):
         print('Loading weights: %s' % start_weights_file)
         model.load_weights(start_weights_file, by_name=True)
         #model = load_model(start_weights_file)
 
 print('Fitting model')
-model.fit_generator(train_generator3(),
+model.fit_generator(train_generator,
                     samples_per_epoch=samples_per_epoch,
-                    validation_data=val_generator3(),
+                    validation_data=val_generator,
                     callbacks=[checkpoint, tensorboard],
                     nb_val_samples=nb_val_samples,
                     nb_epoch=nb_epoch,
--- oscar_garbage_classifier/src/train_squeezenet_detector.py
+++ oscar_garbage_classifier/src/train_squeezenet_detector.py
@@ -14,22 +14,22 @@ import random
 
 initial_epoch = 0
 nb_epoch = 100
-batch_size = 50
-samples_per_epoch = 700
-nb_val_samples = 80
+batch_size = 8
+samples_per_epoch = 750
+nb_val_samples = 130
 
 input_shape = (100, 100, 3)
 width = input_shape[0]
 height = input_shape[1]
 channels = input_shape[2]
 
-training_dir = '../images/training_detector'
-val_dir = '../images/validation_detector'
+training_dir = '../images/training-webcam_detector'
+val_dir = '../images/validation-webcam_detector'
 save_weights_file = '../models/squeezenet_detector_v1.1_weights_%s_epochs_%sx%s.h5' % (nb_epoch, width, height)
 start_weights_file = '' #save_weights_file
 weights_target = "../models/squeezenet_detector_weights_%sx%s.{epoch:02d}-loss_{val_loss:.5f}-acc_{val_acc:.5f}.h5" % (width, height)
 
-classes = ['occupied', 'empty']
+classes = ['empty', 'non_empty']
 nb_classes = len(classes)
 
 train_datagen = ImageDataGenerator(
@@ -38,10 +38,16 @@ train_datagen = ImageDataGenerator(
         width_shift_range=0.15,
         rotation_range=20.,
         height_shift_range=0.15,
+        horizontal_flip=True,
+        vertical_flip=True,  
+        samplewise_std_normalization=True,
+        zca_whitening=True,
         fill_mode='constant')
 
 test_datagen = ImageDataGenerator(
         rescale=1./255,
+        samplewise_std_normalization=True,
+        zca_whitening=True,
         fill_mode='constant')
 
 train_generator = train_datagen.flow_from_directory(training_dir,
@@ -76,15 +82,15 @@ def top_1_categorical_accuracy(y_true, y_pred):
 
 checkpoint = ModelCheckpoint(weights_target, monitor='val_loss',
                              verbose=1, save_best_only=True,
-                             save_weights_only=False, mode='auto')
+                             save_weights_only=True, mode='auto')
 
 tensorboard = TensorBoard(log_dir='../logs', histogram_freq=5, write_graph=True, write_images=False)
 
 print('Loading model..')
 model = SqueezeNet(nb_classes, width, height, channels)
-adam = Adam(lr=0.005)
+adam = Adam(lr=0.002)
 rmsprop = RMSprop(lr=0.005)
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=[top_1_categorical_accuracy, 'accuracy', 'precision', 'recall', 'categorical_crossentropy', 'binary_crossentropy'])
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=[top_1_categorical_accuracy, 'accuracy', 'precision', 'recall', 'categorical_crossentropy', 'binary_crossentropy'])
 if os.path.isfile(start_weights_file):
         print('Loading weights: %s' % start_weights_file)
         model.load_weights(start_weights_file, by_name=True)
